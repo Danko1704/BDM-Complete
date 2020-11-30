f73443f5c86cc01ac2357aa6f2796087068e30be
Orlando Gámez <orlando_gague17@hotmail.com>
2020-11-30 02:12:28 -0600

    CREACION DE NOTICIAS A HUEVO
    ya se crean noticias mi camarada

--- Querys/Procedures.sql
+++ Querys/Procedures.sql
@@ -73,7 +73,7 @@ DELIMITER %$
 CREATE PROCEDURE sp_selectUsuarios(
 )
 Begin
-    SELECT Usuario.nombre FROM Usuario ORDER BY Usuario.nombre ASC;
+    SELECT Usuario.usuarioId, Usuario.nombre FROM Usuario ORDER BY Usuario.nombre ASC;
 END %$
 DELIMITER ;
 
@@ -84,7 +84,7 @@ CREATE PROCEDURE sp_infoUsuario(
 	pNombre varchar(50)
 )
 Begin
-    SELECT Usuario.nombre, Usuario.correo, Usuario.telefono, Usuario.contraseña, Imagen.imagenFile FROM Usuario INNER JOIN Imagen ON Usuario.imagenIdF = Imagen.imagenId WHERE Usuario.nombre = pNombre;
+    SELECT Usuario.usuarioId, Usuario.nombre, Usuario.correo, Usuario.telefono, Usuario.contraseña, Imagen.imagenFile FROM Usuario INNER JOIN Imagen ON Usuario.imagenIdF = Imagen.imagenId WHERE Usuario.nombre = pNombre;
 END %$
 DELIMITER ;
 
@@ -124,13 +124,21 @@ CREATE PROCEDURE sp_agregarNoticia(
     pPalabraClave1 varchar(25),
     pPalabraClave2 varchar(25),
     pPalabraClave3 varchar(25),
-    pAutorId int,
+    pAutor varchar(50),
     pComentarioEditor varchar(1000),
-	pCategoria int
+	pCategoria varchar(50),
+    pEspecial bool
 )
 Begin
-	INSERT INTO Noticia(titulo, sinopsis, fechaCreacion, palabraClave1, palabraClave2, palabraClave3, autorIdF, comentatioEditor, seccionIdF)
-    VALUES (pTitulo, pSinopsis, now(), pPalabraClave1, pPalabraClave2, pPalabraClave3, pAutorId, pComentarioEditor, pCategoria);
+DECLARE idSeccion int;
+DECLARE idAutor int;
+	SET 
+		idSeccion = (SELECT Seccion.seccionId FROM Seccion WHERE Seccion.nombre = pCategoria),
+        idAutor = (SELECT Usuario.usuarioId FROM Usuario WHERE Usuario.nombre = pAutor);
+        
+	INSERT INTO Noticia(titulo, sinopsis, texto, fechaCreacion, palabraClave1, palabraClave2, palabraClave3, autorIdF, comentarioEditor, seccionIdF, especial)
+    VALUES (pTitulo, pSinopsis, pTexto, now(), pPalabraClave1, pPalabraClave2, pPalabraClave3, idAutor, pComentarioEditor, idSeccion, pEspecial);
+    SELECT noticiaId FROM Noticia WHERE noticiaId = LAST_INSERT_ID();
 END %$
 DELIMITER ;
 
@@ -138,21 +146,60 @@ DROP PROCEDURE IF EXISTS sp_agregarCategoria;
 
 DELIMITER %$
 CREATE PROCEDURE sp_agregarCategoria(
-	pTitulo varchar (40),
-	pSinopsis varchar(80),
-    pTexto text,
-    pPalabraClave1 varchar(25),
-    pPalabraClave2 varchar(25),
-    pPalabraClave3 varchar(25),
-    pAutorId int,
-    pComentarioEditor varchar(1000),
-	pCategoria int
+	pNombre varchar(25),
+    pUsuarioIdF int,
+    pColor varchar(7)
 )
 Begin
-	INSERT INTO Noticia(titulo, sinopsis, fechaCreacion, palabraClave1, palabraClave2, palabraClave3, autorIdF, comentatioEditor, seccionIdF)
-    VALUES (pTitulo, pSinopsis, now(), pPalabraClave1, pPalabraClave2, pPalabraClave3, pAutorId, pComentarioEditor, pCategoria);
+	INSERT INTO Seccion(Nombre, usuarioIdF, Color) VALUES (pNombre, pUsuarioIdF, pColor);
 END %$
 DELIMITER ;
+
+DROP PROCEDURE IF EXISTS sp_llenarCategorias;
+
+DELIMITER %$
+CREATE PROCEDURE sp_llenarCategorias(
+
+)
+Begin
+	SELECT Seccion.nombre FROM Seccion ORDER BY Seccion.nombre ASC;
+END %$
+DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE sp_agregarImagenNoticia
+(IN pImagen varchar(100))
+BEGIN
+INSERT INTO Imagen VALUES(0, pImagen);
+SELECT imagenId FROM Imagen WHERE imagenId = LAST_INSERT_ID();
+END //
+DELIMITER ;
+
+
+DELIMITER //
+CREATE PROCEDURE sp_agregarVideoNoticia
+(IN pVideo varchar(100))
+BEGIN
+INSERT INTO Video VALUES(0, pVideo);
+SELECT videoId   FROM Video WHERE videoId  = LAST_INSERT_ID();
+END //
+DELIMITER ;
+
+
+DELIMITER //
+CREATE PROCEDURE sp_llenarMultimedia
+(IN pIdMultimedia int, pNoticiaId int , mediaType int)
+BEGIN
+	if mediaType = 0 THEN
+		INSERT INTO Multimedia VALUES(0, pIdMultimedia, null, pNoticiaId);
+	ELSE
+		INSERT INTO Multimedia VALUES(0, null, pIdMultimedia, pNoticiaId);
+	END if;
+END //
+DELIMITER ;
+
+
+
 
 select * from noticia;
 
@@ -160,17 +207,19 @@ INSERT INTO Usuario(nombre, correo, telefono, contraseña, tipoUsuario) VALUES (
 
 call sp_agregarUsuario('erick', 'lala@hotmail.com', '9512375708', 'dodo');
 call sp_agregarImagenUsuario('lala@hotmail.com','monky_flip.mp4');
-call sp_cambiarReportero ('erick');
+call sp_cambiarReportero ('orlando');
 call sp_selectUsuarios();
-call sp_infoUsuario('papotericudo');
+call sp_llenarCategorias();
+call sp_infoUsuario('danko');
 call sp_imagenUsuarioMostrar('papotericudo');
 call sp_updateUsuario ('papucho', 'perro', 'perro@hotmail.com', '9512278531', 'Peludin9$', 'perro.jpg');
-call sp_agregarNoticia ('huevos', 'muchos huevos', 'huevo1', 'huevo2', 'huevo3', '6', 'no hay pah', '2');
+call sp_agregarNoticia ('Una partida de Huevos', 'hubo muchos huevos', 'se sacaron los calzones', 'huevos', 'juegos', 'Lol', 'orlando', '', 'ps5', false);
+call sp_agregarCategoria('ps5', '1', '#f0f0f0');
+
 select * from Usuario;
 select * from Imagen;
+select * from video;
+select * from Seccion;
+select * from noticia;
 
-SELECT Usuario.nombre, Usuario.correo, Usuario.telefono, Usuario.contraseña, Imagen.imagenFile FROM Usuario INNER JOIN Imagen ON Usuario.imagenIdF = Imagen.imagenId WHERE Usuario.nombre = 'papotericudo';
-
-TRUNCATE TABLE Usuario;
-
-INSERT INTO Imagen(imagenfile) VALUES ("huevos");
+SELECT Usuario.nombre, Usuario.correo, Usuario.telefono, Usuario.contraseña, Imagen.imagenFile FROM Usuario INNER JOIN Imagen ON Usuario.imagenIdF = Imagen.imagenId WHERE Usuario.nombre = 'erick';
